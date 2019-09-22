5b526c7394c941ed7d9301e31af10b4be60d9e7f
Saad Nasir <saad.nasir.pk@gmail.com>
2019-09-22 23:20:11 +0500

    Added thymleaf template for authors

---
+++ src/main/java/guru/springframework/spring5webapp/controller/AuthorController.java
@@ -0,0 +1,26 @@
+package guru.springframework.spring5webapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import guru.springframework.spring5webapp.repository.AuthorRepository;
+
+@Controller
+public class AuthorController {
+
+	private AuthorRepository authorRepo;
+
+	public AuthorController(AuthorRepository authorRepo) {
+		super();
+		this.authorRepo = authorRepo;
+	}
+	
+	@RequestMapping("/authors")
+	public String getAuthors(Model model){
+		 model.addAttribute("authors", authorRepo.findAll());
+		 return "authors";
+	}
+	
+	
+}
